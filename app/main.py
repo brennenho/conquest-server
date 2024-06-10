4ceca39a7ff6a7fba5f349aa9619824ec19456e6
brennenho <brennen.ho@gmail.com>
2024-06-09 22:50:52 -0700

    created postgres db and initial scraping

--- app/main.py
+++ app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+from scrapers.course import CourseParser
+
+scraper = CourseParser()
+scraper.scrape_deparment("csci")
---
+++ app/util/constants.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://classes.usc.edu/"
+TERM = "20243"
+TERM_URL = "term-" + TERM + "/"
