64de85521ed9541fa9aeb4ed0220cd0fc58b07ea
atsai488 <austintsai2014@gmail.com>
2024-06-17 20:29:53 -0700

    moving long constants to constant.py for readablility

--- app/scrapers/rmp.py
+++ app/scrapers/rmp.py
@@ -1,14 +1,7 @@
 import requests
 import json
-import codecs
-
-BASE_URL = "https://www.ratemyprofessors.com/graphql"
-PAYLOAD_PART_1 = '{"query":"query TeacherSearchPaginationQuery(\\n  $count: Int!\\n  $cursor: String\\n  $query: TeacherSearchQuery!\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1jWD3d\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1jWD3d on newSearch {\\n  teachers(query: $query, first: $count, after: $cursor) {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n","variables":{"count":'
-PAYLOAD_PART_2 = ',"cursor":"'
-PAYLOAD_PART_3 = '","query":{"text":"","schoolID":"U2Nob29sLTEzODE=","fallback":true,"departmentID":null}}}'
-
-
-class rmpParser:
+from app.utils.constants import RMP_BASE_URL, RMP_PAYLOAD_HEADER, RMP_PAYLOAD_PART_1, RMP_PAYLOAD_PART_2, RMP_PAYLOAD_PART_3, RMP_FIRST_PAYLOAD
+class RmpParser:
     """
     Scrapes ratemyprofessor using graphql api payloads to load more professors a once.
 
@@ -20,29 +13,14 @@ class rmpParser:
         self.payload = str
         self.cursor = str
         self.parsed_all = False
-        self.headers = {
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Authorization": "Basic dGVzdDp0ZXN0",
-            "Connection": "keep-alive",
-            "Content-Type": "application/json",
-            "Origin": "https://www.ratemyprofessors.com",
-            "Referer": "https://www.ratemyprofessors.com/search/professors/1381?q=*",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-        }
+        self.headers = RMP_PAYLOAD_HEADER
 
     def export_as_json(self):
         """Exports all professors at USC"""
         json_object = json.dumps(
             self.scrape_all_professors(), indent=4, ensure_ascii=False
         )
-        with codecs.open("professors.json", "w", "utf-8") as outfile:
+        with open("professors.json", "w") as outfile:
             outfile.write(json_object)
 
     def scrape_all_professors(self) -> dict:
@@ -66,29 +44,33 @@ class rmpParser:
         Returns:
             dict: formatted json for professor information. See parse_json() for more information.
         """
-        first_payload = '{"query":"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n  $includeSchoolFilter: Boolean!\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) @include(if: $includeSchoolFilter) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\"\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n","variables":{"query":{"text":"","schoolID":"U2Nob29sLTEzODE=","fallback":true,"departmentID":null},"schoolID":"U2Nob29sLTEzODE=","includeSchoolFilter":true}}'
+        first_payload = RMP_FIRST_PAYLOAD
         response = requests.request(
-            "POST", BASE_URL, headers=self.headers, data=first_payload
+            "POST", RMP_BASE_URL, headers=self.headers, data=first_payload
         )
         if response.status_code == 200:
             return self.parse_json(response.json())
         else:
             raise RuntimeError("Error parsing first setup of scrapes")
 
-    def scrape_professors(self, count: str = "10") -> dict:
+    def scrape_professors(self, count: int = 10) -> dict:
         """scrapes professor data from ratemyprofessor
 
         Args:
-            count (str, optional): The number of professor entries to scrape. Defaults to 10.
+            count (int, optional): The number of professor entries to scrape. Defaults to 10.
 
         Returns:
             dict: Formatted json file for professor information. See parse_json() for more information.
         """
         self.payload = (
-            PAYLOAD_PART_1 + count + PAYLOAD_PART_2 + self.cursor + PAYLOAD_PART_3
+            RMP_PAYLOAD_PART_1
+            + str(count)
+            + RMP_PAYLOAD_PART_2
+            + self.cursor
+            + RMP_PAYLOAD_PART_3
         )
         response = requests.request(
-            "POST", BASE_URL, headers=self.headers, data=self.payload
+            "POST", RMP_BASE_URL, headers=self.headers, data=self.payload
         )
         if response.status_code == 200:
             return self.parse_json(response.json())
--- app/utils/constants.py
+++ app/utils/constants.py
@@ -5,3 +5,25 @@ ALLOWED_ORIGINS = [
     "chrome-extension://ijnageikchmnfaoadoiibnncahopcnnh",
     "https://webreg.usc.edu",
 ]
+
+RMP_BASE_URL = "https://www.ratemyprofessors.com/graphql"
+RMP_FIRST_PAYLOAD = '{"query":"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n  $includeSchoolFilter: Boolean!\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) @include(if: $includeSchoolFilter) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\"\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n","variables":{"query":{"text":"","schoolID":"U2Nob29sLTEzODE=","fallback":true,"departmentID":null},"schoolID":"U2Nob29sLTEzODE=","includeSchoolFilter":true}}'
+RMP_PAYLOAD_PART_1 = '{"query":"query TeacherSearchPaginationQuery(\\n  $count: Int!\\n  $cursor: String\\n  $query: TeacherSearchQuery!\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1jWD3d\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1jWD3d on newSearch {\\n  teachers(query: $query, first: $count, after: $cursor) {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n","variables":{"count":'
+RMP_PAYLOAD_PART_2 = ',"cursor":"'
+RMP_PAYLOAD_PART_3 = '","query":{"text":"","schoolID":"U2Nob29sLTEzODE=","fallback":true,"departmentID":null}}}'
+RMP_PAYLOAD_HEADER = {
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Authorization": "Basic dGVzdDp0ZXN0",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json",
+    "Origin": "https://www.ratemyprofessors.com",
+    "Referer": "https://www.ratemyprofessors.com/search/professors/1381?q=*",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+}
