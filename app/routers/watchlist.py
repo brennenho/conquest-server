3fb8c5f21ba9573aa6bedf5b1fac435edb0ddee2
brennenho <brennen.ho@gmail.com>
2024-07-09 20:52:22 -0700

    brennen inital commit

--- app/routers/watchlist.py
+++ app/routers/watchlist.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 
 from app.database.postgres_client import PostgresClient
 from app.alerts.mail import send_watchlist_confirmation
+from app.utils.helpers import formatCourseResponse
 from app.utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -17,7 +18,8 @@ def add(
     try:
         # send_watchlist_confirmation(email, section_id)
         PostgresClient().add_to_watchlist(section_id, department, email)
-        return JSONResponse(content="success", status_code=200)
+        section_info = PostgresClient().search_course_by_id(section_id)
+        return JSONResponse(content=formatCourseResponse(section_info), status_code=200)
     except Exception as e:
         logger.error(f"Error adding to watchlist: {e}")
         return JSONResponse(content="fail", status_code=500)
@@ -34,10 +36,14 @@ def delete(section_id: str = Body(...), email: str = Body(...)) -> JSONResponse:
 
 
 @router.post("/search")
-def search(email: str = Body(...)) -> JSONResponse:
+def get_all(email: str = Body(...)) -> JSONResponse:
     try:
         response = PostgresClient().search_by_email(email=email)
-        return JSONResponse(content=response, status_code=200)
+        sections = {}
+        for section in response:
+            section_info = PostgresClient().search_course_by_id(section[1])
+            sections[section[1]] = formatCourseResponse(section_info)
+        return JSONResponse(content=sections, status_code=200)
     except Exception as e:
-        logger.error(f"Error searching watchlist: {e}")
+        logger.error(f"Error getting all watchlist: {e}")
         return JSONResponse(content=False, status_code=500)
