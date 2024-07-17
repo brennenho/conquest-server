7eb619122dfe74aafd1ffd303723ae2ea283f848
atsai488 <austintsai2014@gmail.com>
2024-07-17 14:59:19 -0700

    json response for search router

--- app/routers/search.py
+++ app/routers/search.py
@@ -54,5 +54,5 @@ def search_course(course: str = Body(..., embed=True)):
     client = PostgresClient()
     result = client.search_course(course)
     if len(result) == 0:
-        return Response(status_code=204)
+        return JSONResponse(content={"valid": False}, status_code=404)
     return JSONResponse(content={"valid": True, "result": result}, status_code=200)
